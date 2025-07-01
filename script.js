b14fb432fae5b592c05dc6894f26ba14cfc1bfaa
diminishedcaustic <157384379+diminishedcaustic@users.noreply.github.com>
2025-07-01 22:03:22 +0800

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+const sizeFactor = 104;
+const changeSizeBtn = document.querySelector('#change-size-btn');
+const mainContent = document.querySelector('.main-content');
+
+let size;
+
+changeSizeBtn.addEventListener('click', () => {
+    size = prompt('Enter size: ');
+    mainContent.style.width = size*sizeFactor + 'px';
+    removeSquares();
+    addSquares();
+});
+
+function addSquares() {
+    for(let i = 0; i < size*size; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'red';
+        });
+        mainContent.append(square);
+    }
+}
+
+function removeSquares() {
+    mainContent.querySelectorAll('div').forEach((child) => {
+        child.parentElement.removeChild(child);
+    });
+}
+
+function changeSquareColor() {
+
+}
